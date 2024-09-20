32fc19781bd939676c7cddb513017e9c2ba93791
Ciel <Ciel@gmail.com>
2024-09-20 18:22:29 -0300

    Desafío 2

---
+++ Desafío 2/index.js
@@ -0,0 +1,4 @@
+const nombreProducto = "Pulseras DIY";
+const precioUnitario = 1000;
+let cantidadDeseada = Number(prompt("Ingrese la cantidad de productos que quiera comprar."));
+alert(`El precio de su compra es ${precioUnitario * cantidadDeseada}`);
