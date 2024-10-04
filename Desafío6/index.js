0ac297e821cf7f0745f359fb75ecff18d5d5650f
Ciel <Ciel@gmail.com>
2024-10-04 19:19:46 -0300

    Desafío 6

---
+++ Desafío6/index.js
@@ -0,0 +1,12 @@
+let producto = {
+    nombre: "Camiseta",
+    precio: 1200,
+    cantDisponible: 10,
+};
+
+producto.categoría = "Ropa"
+
+producto.nombre = "Zapatillas"
+producto.categoría = "Calzado"
+
+delete producto.cantDisponible
